79376b95e9c4bd306a7b65394aee43e4dd2729bf
Serhii Myshko <sergeiown@gmail.com>
2023-04-17 21:59:39 +0300

    add quick sort description

--- 5_quick_sort.js
+++ 5_quick_sort.js
@@ -1,3 +1,12 @@
+/* The quickSort function takes an array as a parameter.
+If the array contains one element or less, the function returns this array.
+If the array contains more than one element, the pivot element is selected. In this case, the average value of the array is used as the pivot element, finding the index that divides the length of the array in half.
+Two empty subsets are created - smaller than pivot (less) and larger than pivot (greater).
+The array is iterated, and each element is compared to pivot. If the element is smaller than pivot, it is added to the less array, otherwise - to the greater array.
+For each of the subsets less and greater, the quickSort function is called recursively.
+The sorted subarrays less, pivot and greater are combined into one sorted array.
+The sorted array is returned as the result of the quickSort function. */
+
 const arr = [3, 0, 2, 5, -1, 4, 1, 15, -8, 7, 6, 9, 8, 11, 10, 13, 12, 14];
 let count = 0;
 
